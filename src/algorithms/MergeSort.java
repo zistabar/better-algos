d501e73ea3f8aafe6edbc135ec9fe69f3397b7a6
Andrzej Zieta <andrzej.zieta@gmail.com>
2023-07-02 22:24:36 +0200

    Add quickelect and mergesort

---
+++ src/algorithms/MergeSort.java
@@ -0,0 +1,51 @@
+public class MergeSort {
+
+    void mergeSort(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private void mergeSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+
+            merge(nums, left, mid, right);
+        }
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        int leftLength = mid - left + 1;
+        int rightLength = right - mid;
+
+        int[] leftArray = new int[leftLength];
+        int[] rightArray = new int[rightLength];
+
+        for (int i = 0; i < leftLength; i++) {
+            leftArray[i] = nums[left + i];
+        }
+        for (int i = 0; i < rightLength; i++) {
+            rightArray[i] = nums[mid + 1 + i];
+        }
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < leftLength && j < rightLength) {
+            if (leftArray[i] <= rightArray[j]) {
+                nums[k++] = leftArray[i++];
+            } else {
+                nums[k++] = rightArray[j++];
+            }
+        }
+        while (i < leftLength) {
+            nums[k++] = leftArray[i++];
+        }
+        while (j < rightLength) {
+            nums[k++] = rightArray[j++];
+        }
+    }
+
+
+}
---
+++ src/algorithms/QuickSelect.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+public class QuickSelect {
+
+    /**
+     *  k-th the largest element in the array
+     *  Runtime: O(log(n))
+     *  Space: O(n)
+     */
+    int quickSelect(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if (left == right) {
+            return nums[left];
+        }
+        int pivotIdx = new Random().nextInt(right - left + 1) + left;
+
+        pivotIdx = partition(nums, left, right, pivotIdx);
+
+        if (pivotIdx == k) {
+            return nums[k];
+        } else if (pivotIdx < k) {
+            return quickSelect(nums, pivotIdx + 1, right, k);
+        } else {
+            return quickSelect(nums, left, pivotIdx - 1, k);
+        }
+    }
+
+    private int partition(int[] nums, int left, int right, int pivotIdx) {
+        int pivot = nums[pivotIdx];
+
+        swap(nums, pivotIdx, right);
+
+        pivotIdx = left;
+
+        for (int i = left; i <= right; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, i, pivot++);
+            }
+        }
+        return pivotIdx - 1;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int cachedA = nums[a];
+
+        nums[a] = nums[b];
+        nums[b] = cachedA;
+    }
+
+}
