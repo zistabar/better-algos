0a4503c41778ee06e7e6e57d66f917fe78ab3525
Andrzej Zieta <andrzej.zieta@gmail.com>
2024-02-19 22:17:48 +0100

    Simplify and complete token bucket algorithm

--- src/algorithms/TokenBucket.java
+++ src/algorithms/TokenBucket.java
@@ -6,19 +6,22 @@ public class TokenBucket {
     private final long maxBucketSize;
     private final long refillRate;
 
-    private double currentBucketSize;
+    private double bucketSize;
     private long lastRefillTimestamp;
 
     public TokenBucket(long maxBucketSize, long refillRate) {
         this.maxBucketSize = maxBucketSize;
         this.refillRate = refillRate;
+
+        bucketSize = maxBucketSize;
+        lastRefillTimestamp = System.nanoTime();
     }
 
     public synchronized boolean allowRequest(int tokens) {
         refill();
 
-        if (currentBucketSize > tokens) {
-            currentBucketSize -= tokens;
+        if (bucketSize > tokens) {
+            bucketSize -= tokens;
 
             return true;
         }
@@ -29,7 +32,7 @@ public class TokenBucket {
         long now = System.nanoTime();
         double tokensToRefill = (now - lastRefillTimestamp) * refillRate / 1e9;
 
-        currentBucketSize = Math.min(maxBucketSize, currentBucketSize + tokensToRefill);
+        bucketSize = Math.min(maxBucketSize, bucketSize + tokensToRefill);
         lastRefillTimestamp = now;
     }
 }
